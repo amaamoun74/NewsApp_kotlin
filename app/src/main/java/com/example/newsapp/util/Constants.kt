e32855c2b2d9701d52c9614d48bc7434c6b36980
Ahmed Maamoun <82960902+amaamoun74@users.noreply.github.com>
2022-11-17 19:48:30 +0200

    Update Constants.kt

--- app/src/main/java/com/example/newsapp/util/Constants.kt
+++ app/src/main/java/com/example/newsapp/util/Constants.kt
@@ -1,12 +1,13 @@
 package com.example.newsapp.util
 
+// this class for constant values
 class Constants {
     companion object {
-        const val API_KEY = "791dc72a7bca411b9b212cfcd64e7b4f"
+        const val API_KEY = "" // add your own api key
         const val BASE_URL = "https://newsapi.org/"
         const val PAGE_SIZE = 15
         const val COUNTRY_CODE = "eg"
       //  const val NEWS_ARTICLE_KEY ="article"
-        const val TAG = "logcat"
+        const val TAG = "logcatInfo"
     }
 }
